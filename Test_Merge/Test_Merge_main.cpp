d992956f12281f332a79d2d0360bf4bd22ab6c60
mike <guliaevm@yahoo.com>
2020-07-16 17:35:42 +0400

    Test - V01

---
+++ Test_Merge/Test_Merge_main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Test_Merge_main.cpp
+ * Author: mike
+ *
+ * Created on July 16, 2020, 5:33 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "Hello, World! - V01" << endl;
+
+    return 0;
+}
